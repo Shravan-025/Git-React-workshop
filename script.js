d3444ebe51e246a409d2018ef932aa2537142288
patelshravan0808@gmail.com <pranshukalathia383@gmail.com>
2025-11-01 11:50:44 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+let namee = "Shravan"
+var age = 18;
+
+
+console.log(namee);
+console.log(age);
+console.table( {age, namee});
+
+let a=2
+let b="2"
+
+console.log(a==b)
+console.log(a===b)
